4bc68ad13c4f63571e298f1f89e0efe814d12b8d
Abdulqadir <a.q.r@hotmail.co.uk>
2018-07-15 19:02:19 +0100

    add player smooth rotation

---
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float shipSpeed = 5f;
+	public float rotationSpeed = 300f;
+	private Rigidbody2D rigidBody;
+
+	void Awake(){
+		rigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	void Start () {
+
+	}
+	
+	void Update () {
+		ShipTurn();
+	}
+
+	void ShipTurn(){
+
+		Quaternion rotation = transform.rotation;
+		float z = rotation.eulerAngles.z;
+		z -= Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+		rotation = Quaternion.Euler(0,0,z);
+		transform.rotation = rotation;
+	}
+}
