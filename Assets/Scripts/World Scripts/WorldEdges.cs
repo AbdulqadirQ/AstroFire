e4e00b0c3db5b41c8f2872459fa3f425ff457c4c
Abdulqadir <a.q.r@hotmail.co.uk>
2018-07-16 21:59:55 +0100

    add teleport-on-trigger world edges for wrapped game world

---
+++ Assets/Scripts/World Scripts/WorldEdges.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldEdges : MonoBehaviour {
+
+	private GameObject object_in_collision;
+	private string edge;
+
+	void OnTriggerEnter2D(Collider2D target){
+		if(target){
+			object_in_collision = target.gameObject;
+			float xPos = object_in_collision.transform.position.x;
+			float yPos = object_in_collision.transform.position.y;
+
+			edge = gameObject.name;
+
+			if(edge == "TopEdge"){
+				object_in_collision.transform.position = new Vector3(xPos,-5.5f,0);
+			}else if(edge == "BottomEdge"){
+				object_in_collision.transform.position = new Vector3(xPos,5.5f,0);
+			}else if(edge == "LeftEdge"){
+				object_in_collision.transform.position = new Vector3(9f,yPos,0);
+			}else if(edge == "RightEdge"){
+				object_in_collision.transform.position = new Vector3(-9f,yPos,0);
+			}
+		}
+	}
+}
